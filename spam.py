c2138494df542106a2082bed790bac5a9d374bd7
rodinam <rodinam@lad24.ru>
2022-04-11 12:21:53 +0300

    init commit

---
+++ spam.py
@@ -0,0 +1,19 @@
+import json
+
+
+def spam_function(arr_str_json: str) -> str:
+    if not isinstance(arr_str_json, str):
+        raise TypeError('Parameter of function must be string')
+    try:
+        arr_json = json.loads(arr_str_json)
+    except json.decoder.JSONDecodeError:
+        raise ValueError('Incorrect json format')
+    if 'arr' in arr_json:
+        if len(arr_json['arr']) >= 2:
+            if len(arr_json['arr'][1]) >= 3:
+                print('The array changed')
+                arr_json['arr'][1][2] = 9
+    return json.dumps(arr_json)
+
+
+# print(spam_function('{"arr": [[1,2,3],[2,4,5]]}'))
