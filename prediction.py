a7c615afec5929f4aacde9465031bda3babf16b9
rodinam <rodinam@lad24.ru>
2022-04-13 15:47:01 +0300

    change return value in prediction

--- prediction.py
+++ prediction.py
@@ -3,14 +3,14 @@ import sys
 import json
 
 
-def prediction(input_data: str) -> list[[float]]:
+def prediction(input_data: str) -> str:
     try:
         input_data = json.loads(input_data)
     except json.decoder.JSONDecodeError:
         print("Type error: incorrect input data")
-        return [[0.0], [0.0]]
+        return '[0.0, 0.0]'
     time.sleep(1)
-    result = [[2.3, 5.2], [532.21, 13.22]]
+    result = '[2.3, 5.2, 532.21, 13.22]'
     print(result)
     return result
 
